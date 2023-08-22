221405929a97487957a76b4fda81eef059c1c83c
sam <shixiaomu2000@gmail.com>
2023-08-22 14:54:31 +0800

    asdfa

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::fs::File;
+use std::io::{ErrorKind, Read};
 
-
-fn  main() {
-   
+fn main() {
+    let f: File = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+    let mut buf = String::new();
+    f.read_to_string(&mut  buf);
+    println!("{}",buf)
 }
