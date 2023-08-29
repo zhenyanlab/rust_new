ee448eb735b0f127b5a83eb0b1b0c3610b4f0634
sam <shixiaomu2000@gmail.com>
2023-08-29 14:37:50 +0800

    ad

---
+++ tests/boxtest.rs
@@ -0,0 +1,43 @@
+#[test]
+fn boxTest(){
+    let  i = 5;
+    let bi = Box::new(i);
+    //装箱和解引用
+    assert_eq!(bi, Box::new(5));
+    assert_eq!(*bi, 5);
+    println!("{}",bi);
+    //闭包和lamada表达式
+    let fn1 = |i : u8| i+2;
+    let b = fn1(3);
+    println!("boxTest:{}",b);
+
+
+}
+struct Node{
+    val : u8,
+    next:Option<Box<Node>>,
+}
+impl Node{
+    fn push(&mut self,node :Node){
+        
+    }
+}
+fn linktest(){
+    let mut root = Node{val:0,next:None};
+    let mut one = Node{val:1,next:None};
+    let two = Node{val:2,next:None};
+    root.next = Some(Box::new(one));
+    one.next = Some(Box::new(two));
+
+    foreachLink(&root);
+
+
+}
+fn foreachLink(root : &Node){
+    let mut iRoot = root;
+    println!("{}",iRoot.val);
+    // while(iRoot.next != None){
+        let mut iRoot=iRoot.next.as_ref();   
+        foreachLink(iRoot.unwrap());
+    // }
+}
--- tests/closefn.rs
+++ tests/closefn.rs
@@ -1,13 +1,12 @@
+#[derive(Debug)]
 struct  P{
     id :i32,
     next :Option<Box<P>>,
 }
 
-fn print_type_of<T>(_: &T) {
-
-        println!("TYPE-PRINT:{}", std::any::type_name::<T>())
-    
-    }
+fn print_type_of<T>(log : &str,_: &T) {
+        println!("##{}##:{}", log,std::any::type_name::<T>())
+}
 
 
 //* 
@@ -18,14 +17,14 @@ fn print_type_of<T>(_: &T) {
 //判断None不能用!=和== 而是用方法..is_none
  //*/
 fn print_link(p:P){
-    print_type_of(&p);
+    print_type_of("print_link",&p);
     println!("{}",p.id);
     let mut pp = p;
     while !pp.next.is_none() {
         let vv = pp.next;
-        print_type_of(&vv);
+        print_type_of("print_link_VV",&vv);
         pp = *(vv.unwrap());
-        print_type_of(&pp);
+        print_type_of("print_link_pp",&pp);
         println!("{}",pp.id);
     }
 
@@ -35,17 +34,33 @@ fn closefn(){
     let a: Option<Box<i32>> = Some(Box::new(5));
     let mut b = a.unwrap();
     *b = 8;
-    print_type_of(&b);
+    print_type_of("close_fn",&b);
     println!("{}",b);
     println!("{}",*b);
     println!("{}",&b);
+
     let mut rootP = P{id:0,next:None};
     let mut p1 = P{id:1,next:None};
     let p2 = P{id:2,next:None};
-    print_type_of(&rootP);
+    // 这里的等号是单等号,也是判断相等的意思.
+    if let P{id:4,next:None}=p2{
+        //pass
+    }
+    let thread = Some(3);
+    // 这里的等号是单等号,也是判断相等的意思.
+    //true
+    if let Some(3)= thread{
+        println!("if let math express{:?}",Some(3));
+    }
+    print_type_of("rootp",&rootP);
+
     let mut bp1: Box<P> = Box::new(p1);
+    //rust内存对象的装盘,必须按照一定的次序进行,先组装部分内存对象,再组装整体内存对象,
+    //部分内存对象装配到整体内存对象后,就不能再动了?
     bp1.next=Some(Box::new(p2));
     rootP.next=Some(bp1);
+
+
     //error
     // print_link(*bp1);
     print_link(rootP);
